bb34e26d8ea66aff0c8b84000a66eae1921f3fe2
Bartłomiej Kisiel <bartlomiejkisi3l@gmail.com>
2020-08-22 14:16:30 +0200

    thymeleaf update + core-layout with jQuery import added

--- src/main/java/bartkisiel/github/io/HotelBooking/hotel/HotelController.java
+++ src/main/java/bartkisiel/github/io/HotelBooking/hotel/HotelController.java
@@ -2,11 +2,12 @@ package bartkisiel.github.io.HotelBooking.hotel;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HotelController {
     private final HotelRepository repository;
 
